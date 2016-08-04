480310a3c0e77de1578e8365036780bb97a8aa82
Doug <dougie+github@freshprogramming.com>
2016-08-04 01:49:41 -0700

    Remove old DB test code

--- FreshTools/code/Systems/NetworkMonitor.cs
+++ FreshTools/code/Systems/NetworkMonitor.cs
@@ -433,32 +433,6 @@ namespace FreshTools
             return pageExists;
         }
 
-        public void TestWriteToDB()
-        {
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.ConnectionString = "Server=[server_name];Database=[database_name];Trusted_Connection=true";
-                conn.Open();
-
-                // Create the command
-                SqlCommand command = new SqlCommand("SELECT * FROM TableName WHERE FirstColumn = @firstColumnValue", conn);
-                // Add the parameters.
-                command.Parameters.Add(new SqlParameter("firstColumnValue", 1));
-
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    // while there is another record present
-                    while (reader.Read())
-                    {
-                        // write the data on to the screen
-                        Console.WriteLine(String.Format("{0} \t | {1} \t | {2} \t | {3}",
-                            // call the objects from their index
-                        reader[0], reader[1], reader[2], reader[3]));
-                    }
-                }
-            }
-        }
-
         public static bool IsTheInternetUp()
         {
             PingReply pr = Ping(InternetURL);
